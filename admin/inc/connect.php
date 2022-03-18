f44bffb901bb260bc8ade57057892557fc144fd3
Muxriddin <58684555+Muxriddin8687@users.noreply.github.com>
2022-03-18 16:39:33 +0500

    file

--- admin/inc/connect.php
+++ admin/inc/connect.php
@@ -19,7 +19,7 @@ function test_input($data)
 {
     $data = trim($data);
     $data = stripslashes($data);
-    $data = htmlspecialchars($data);
+    $data = htmlspecialchars($dota);
     $data = filter_var($data, FILTER_SANITIZE_STRING);
 
     return $data;
